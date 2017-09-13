40c5ac67b5e6bda2d45098ed2b6e4b261257772c
speug <lauri.seppalainen@aalto.fi>
2017-09-13 12:29:32 +0300

    1 done sans saving

--- ex1/rgb_scatter.py
+++ ex1/rgb_scatter.py
@@ -1,6 +1,24 @@
 import os
-import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
 
-
-
+files = os.listdir("materials/Webcam/")
+rs = []
+gs = []
+for f in files:
+    im = Image.open("materials/Webcam/" + f)
+    pixs = im.load()
+    s = im.size
+    r = 0
+    g = 0
+    for x in range(0,s[0]):
+        for y in range(0,s[1]):
+            pix = pixs[x,y]
+            r = r + pix[0]
+            g = g + pix[1]
+    rs.append(r)
+    gs.append(g)
+plt.scatter(rs,gs)
+plt.ylabel("Sum of greens")
+plt.xlabel("Sum of reds")
+plt.show()
